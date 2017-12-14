59f157643d19d2f55a67a68dd4b7d2b5a8499829
Tahir Aziz <tahir.aziz4@gmail.com>
2017-12-13 19:31:00 -0600

    added changing name and desc

--- src/containers/PizzaView/index.js
+++ src/containers/PizzaView/index.js
@@ -6,9 +6,11 @@ export class PizzaView extends Component {
   constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleOrder = this.handleOrder.bind(this);
     this.state = {
-      pizza: {}
+      customPizza: {}
     };
+    this.pizza = {};
   }
   handleInputChange(e, cat) {
     const target = e.target;
@@ -16,15 +18,27 @@ export class PizzaView extends Component {
       target.type === "checkbox" || target.type === "radio"
         ? target.checked
         : target.value;
-    const name = target.name;
-    let newPizza = this.state.pizza;
-    let toppingCat = cat.toLowerCase();
-    console.log();
-    newPizza[toppingCat][name] = value;
+    const name = target.type === "radio" ? target.value : target.name;
+    let newPizza = this.state.customPizza;
+    let category = cat.toLowerCase();
+    if (cat === "Crust") {
+      for (const crust in newPizza[category]) {
+        if (crust === name) {
+          newPizza[category][crust] = true;
+        } else {
+          newPizza[category][crust] = false;
+        }
+      }
+    } else if (cat === "name" || cat === "description") {
+      newPizza[category] = value;
+    } else {
+      newPizza[category][name] = value;
+    }
     this.setState({
-      pizza: newPizza
+      customPizza: newPizza
     });
   }
+  handleOrder() {}
   componentDidMount() {
     const pizzaRef = firebase
       .database()
@@ -42,8 +56,9 @@ export class PizzaView extends Component {
         veggies: pizza.veggies
       };
       this.setState({
-        pizza: newState
+        customPizza: newState
       });
+      this.pizza = newState;
     });
   }
   renderToppingInputs(toppingCategory, topping) {
@@ -51,17 +66,32 @@ export class PizzaView extends Component {
 
     for (let choice in topping) {
       //male separate check for type=radio for crust
-      inputs.push(
-        <label key={choice}>
-          <input
-            name={choice}
-            type="checkbox"
-            checked={topping[choice] === true}
-            onChange={e => this.handleInputChange(e, toppingCategory)}
-          />
-          {choice}
-        </label>
-      );
+      if (toppingCategory === "Crust") {
+        inputs.push(
+          <label key={choice}>
+            <input
+              name={toppingCategory}
+              value={choice}
+              type="radio"
+              checked={topping[choice] === true}
+              onChange={e => this.handleInputChange(e, toppingCategory)}
+            />
+            {choice}
+          </label>
+        );
+      } else {
+        inputs.push(
+          <label key={choice}>
+            <input
+              name={choice}
+              type="checkbox"
+              checked={topping[choice] === true}
+              onChange={e => this.handleInputChange(e, toppingCategory)}
+            />
+            {choice}
+          </label>
+        );
+      }
     }
     return inputs;
   }
@@ -76,20 +106,39 @@ export class PizzaView extends Component {
   render() {
     const toppingCategories = ["Crust", "Cheeses", "Meats", "Veggies"];
     let pizzaToppings = [
-      this.state.pizza.crust,
-      this.state.pizza.cheeses,
-      this.state.pizza.meats,
-      this.state.pizza.veggies
+      this.state.customPizza.crust,
+      this.state.customPizza.cheeses,
+      this.state.customPizza.meats,
+      this.state.customPizza.veggies
     ];
+    const pizzaName = Object.keys(this.state.customPizza)[0];
+    const pizzaDesc = Object.keys(this.state.customPizza)[2];
     return (
       <section className="pizzaView">
         <div className="pizzaView__description">
-          <h2>{this.state.pizza.name}</h2>
-          <h3>{this.state.pizza.description}</h3>
-          <h3>{this.state.pizza.pizzams}</h3>
+          <h2>{this.state.customPizza.name}</h2>
+          <h3>{this.state.customPizza.description}</h3>
+          <label>
+            {pizzaName}
+            <textarea
+              name={pizzaName}
+              value={this.state.customPizza.name}
+              onChange={e => this.handleInputChange(e, e.target.name)}
+            />
+          </label>
+          <label>
+            {pizzaDesc}
+            <textarea
+              name={pizzaDesc}
+              value={this.state.customPizza.description}
+              onChange={e => this.handleInputChange(e, e.target.name)}
+            />
+          </label>
+          <h3>{this.state.customPizza.pizzams} Pizzams</h3>
         </div>
         <form className="pizzaView__toppings">
           {this.renderToppings(pizzaToppings, toppingCategories)}
+          <input type="submit" value="Order 🍕" onClick={this.handleOrder} />
         </form>
       </section>
     );
